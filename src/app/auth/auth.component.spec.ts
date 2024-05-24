b56d7d8c83bdf60dd41c255af51019be5f286b8e
jmlc643 <josemarialuyocampos@gmail.com>
2024-05-23 23:56:26 -0500

    NutriCampus-2 Ultimo push

--- src/app/auth/auth.component.spec.ts
+++ src/app/auth/auth.component.spec.ts
@@ -23,7 +23,7 @@ describe('AuthComponent', () => {
 
   it('should display a success message on correct login', () => {
     component.loginForm.setValue({
-      username: 'testtuser',
+      username: 'testuser',
       password: 'password'
     });
     component.login();
